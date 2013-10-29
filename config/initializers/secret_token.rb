32188bff8b1d8c4d40ad8bbd1c390d6a713ce1a3
Toh Yong Cheng <tohyongcheng@gmail.com>
2013-10-29 14:18:19 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ideabakery::Application.config.secret_key_base = 'b377cef1cb2dd8ed8e72ad772ce3b7eea0c8aa488f240776af5690d26e2ab898b32cba37599a89c019c144fc43f2c06e59b88e643b8fbf0458a29cc4c489d74e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ideabakery::Application.config.session_store :cookie_store, key: '_ideabakery_session'
